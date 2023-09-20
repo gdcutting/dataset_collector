6685fafbb7aee2d26cadbe2f74e5b0a56be34c96
Guy Cutting <guy.cutting@gmail.com>
2023-09-20 12:21:21 -0700

    added functions

--- downloader.py
+++ downloader.py
@@ -4,43 +4,120 @@ import os
 import kaggle
 import json
 import yaml
+import re
 from bs4 import BeautifulSoup
+from json.decoder import JSONDecodeError
 
-# using the Kaggle API to generate candidate downloads
-# authentication is handled automatically by the kaggle package
-# you will need to go to the kaggle website and create an API token
-# see https://github.com/Kaggle/kaggle-api under 'API Credentials'
+# set default config values
+# will deprecate at some point
+download_limit_mb = 20
+max_kaggle_downloads = 3
 
-# fetch list of datasets - default sort is 'hottest' so this will fetch datasets of current interest/activity
-dataset_list = kaggle.api.dataset_list()
-for dataset in dataset_list[0:3]:
-    # print(dataset)
-    # print(vars(dataset))
-    print("Downloading from " + dataset.ref)
-    download_path = "datasets/kaggle" + dataset.ref
-    kaggle.api.dataset_download_files(dataset.ref, path=download_path,unzip=True)
+# instantiate tracking
+download_refs = {}
+
+def read_config():
+    """
+    read_config() reads the configuration values from config.yaml
+    """
+    # read the user's credentials from the config file
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    # set the file size limit from the config
+    print('Download limit: ' + str(config['download_limit_mb']) + "MB")
+    download_limit_mb = config['download_limit_mb']
+
+    # set max kaggle downloads
+    max_kaggle_downloads = config['sources']['kaggle']['max_session_downloads']
+
+def read_dataset_status():
+    """
+    read_dataset_status() checks datasets.json to get lists of downloaded datasets
+    """
+    # open JSON file
+    file = open('datasets.json')
     
+    # read JSON file into dict
+    datasets = json.load(file)  
 
-# list using search
-# need try/except block because will error if no records returned
-datasets_searched = kaggle.api.datasets_list(search="air quality")
+    # Getting the list of values of the dictionary with the list comprehension
+    # demoDictionary[dict_key] represents dictionary value
+    datasets_list = [datasets[dict_value] for dict_value in datasets]
+    print(datasets_list)
 
-# Define the URL of the webpage you want to scrape
-url = "https://example.com/datasets"
 
-# Send an HTTP GET request to the URL
-response = requests.get(url)
 
-# Parse the HTML content of the page using Beautiful Soup
-soup = BeautifulSoup(response.text, 'html.parser')
+# default Kaggle download - fetch small number of 'hot' datasets
+def kaggle_download():
+    """
+    kaggle_download() performs downloads from the kaggle data source.
+    Uses the Kaggle API to generate candidate downloads.
+    Authentication is handled automatically by the kaggle package
+    You will need to go to the kaggle website and create an API token.
+    See https://github.com/Kaggle/kaggle-api under 'API Credentials'.
+    """
 
-# Find links to datasets on the page
-dataset_links = []
-for link in soup.find_all('a'):
-    if 'dataset' in link.get('href'):
-        dataset_links.append(link.get('href'))
+    # fetch list of datasets - default sort is 'hottest' so this will fetch datasets of current interest/activity
+    dataset_list = kaggle.api.dataset_list()
 
-# Print the dataset links
-for link in dataset_links:
-    print(link)
+    #iterate through dataset list and download relevant files
+    for dataset in dataset_list[0:max_kaggle_downloads]:
+        # print("Checking " + dataset.ref + "\n")
+        # print("Size: " + dataset.size + "\n")
+        if filesize_check(dataset.size):
+            # print("Passed size check\n")
+            # print(vars(dataset))
+
+            # set source-appropriate download directory
+            download_path = "datasets/kaggle/" + dataset.ref
+            dataset_files = kaggle.api.dataset_list_files(dataset.ref).files
+            dataset_size = dataset.size
+            print("Downloading from " + dataset.ref + " (" + dataset.size + ")")
+            print("Dataset files: " + str(dataset_files))
+            # log_download("kaggle",dataset.ref,dataset.size,str(dataset.files))
+            #kaggle.api.dataset_download_files(dataset.ref, path=download_path, unzip=True, quiet=False)
+        else:
+            print(dataset.ref + " failed size check. Skipping.")
+        
+        # print("---------")
+
+def separate_num_char(s):
+    """
+    separate_num_chars(s) accepts a string containing letters and numbers and splits it into numeric and character components
+    """
+    res = re.split('([-+]?\d+\.\d+)|([-+]?\d+)', s.strip())
+    res_f = [r.strip() for r in res if r is not None and r.strip() != '']
+    return res_f
+
+def filesize_check(filesize):
+    """
+    filesize_check() compares filesize to the download_limit_mb which can be set in config.yaml
+    """
+    size, unit = separate_num_char(filesize)
+    size = int(size)
+
+    if unit == 'KB':
+        return True
+    elif unit == 'MB':
+        if size <= download_limit_mb:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def track_dataset():
+    # JSON file
+    return 0
+
+def main():
+    # read_status()
+    read_config()
+    kaggle_download()
+
+if __name__ == "__main__":
+    main()
+
+
 
