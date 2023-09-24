71faf5fab2a4d68f3ad695ecbe33223a0d0006bc
Guy Cutting <guy.cutting@gmail.com>
2023-09-23 23:47:50 -0700

    initial creation of uploader.py

---
+++ uploader.py
@@ -0,0 +1,12 @@
+"""
+File: uploader.py
+Author: Guy Cutting
+Date: 9/23/2023
+Description: uploads files already downloaded with downloader, to a GitHub repo or elsewhere
+"""
+
+def main():
+	print("This is uploader.py.")
+
+if __name__ == "__main__":
+	main()
