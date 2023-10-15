ee366ef1f838ff3fe0f71053298d8598444a6f91
Guy Cutting <guy.cutting@gmail.com>
2023-10-14 22:59:02 -0700

    corrected order of comments

--- uploader.py
+++ uploader.py
@@ -38,10 +38,9 @@ def check_upload_status():
 
 	print("There are currently " + str(row_count) + " downloads:")
 	for row in rows:
-    # Each row is a tuple, and you can access columns by index
+    	# Each row is a tuple, and you can access columns by index
 		column1_value = row[0]
 		column2_value = row[1]
-		# ... and so on for other columns
 
     	# You can also access columns by name if you use a named tuple
     	# (assuming your query returned named columns)
